a300e910ee81a5748f5afa32faba53c91129ea94
Nick Schwab <nickschwab122@gmail.com>
2022-06-25 19:15:06 -0400

    Added notes field to Workout data model

--- Iron/CoreData/Workout+CoreDataProperties.swift
+++ Iron/CoreData/Workout+CoreDataProperties.swift
@@ -11,52 +11,51 @@ import CoreData
 
 
 extension Workout {
-
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Workout> {
         return NSFetchRequest<Workout>(entityName: "Workout")
     }
-
-    @NSManaged public var name: String?
+    
     @NSManaged public var id: UUID?
-    @NSManaged public var exercise: NSSet?
-
+    
+    
+    @NSManaged public var name: String?
     public var wrappedName: String {
         name ?? "Unknown Name"
     }
     
+    @NSManaged public var notes: String?
+    public var strNotes: String? {
+        get {notes ?? ""}
+        set {notes = newValue}
+    }
+    
+    @NSManaged public var exercise: NSSet?
     public var exerciseArray: [Exercise] {
-        
-        get {
-            let set = exercise as? Set<Exercise> ?? []
-            return set.sorted {
-                $0.wrappedName < $1.wrappedName
-            }
+        let set = exercise as? Set<Exercise> ?? []
+        return set.sorted {
+            $0.wrappedName < $1.wrappedName
         }
-//        set {
-//            
-//            self.exerciseArray = newValue
-//        }
-
     }
 }
 
 // MARK: Generated accessors for exercise
 extension Workout {
-
+    
     @objc(addExerciseObject:)
     @NSManaged public func addToExercise(_ value: Exercise)
-
+    
     @objc(removeExerciseObject:)
     @NSManaged public func removeFromExercise(_ value: Exercise)
-
+    
     @objc(addExercise:)
     @NSManaged public func addToExercise(_ values: NSSet)
-
+    
     @objc(removeExercise:)
     @NSManaged public func removeFromExercise(_ values: NSSet)
-
+    
 }
 
 extension Workout : Identifiable {
-
+    
 }
